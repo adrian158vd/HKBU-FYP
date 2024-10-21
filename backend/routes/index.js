5d52066e90d2e07d6bee4c92d3c3d7caa5c2ad1b
KamWingOn <23213582@life.hkbu.edu.hk>
2024-10-21 14:46:42 +0800

    ok

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+
+/* Handle the Form */
+router.post('/bookings', async function (req, res) {
+
+  var response = {
+      headers: req.headers,
+      body: req.body
+  };
+
+  res.json(response);    
+});
---
+++ backend/utils/db.js
@@ -0,0 +1,18 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+process.env.MONGODB_URI = 'mongodb://adrian158vd:CtZtwlbIdebpFTUfNa6KRiIqGXUJthY4S9Xw2uZX2PJEAmxeGdmInKvMIqxjxgU5Xmqh4tFNXPr2ACDbYGnzrA==@adrian158vd.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@adrian158vd@';
+
+if (!process.env.MONGODB_URI) {
+    // throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+    process.env.MONGODB_URI = 'mongodb://localhost:27017';
+}
+
+// Connect to MongoDB
+async function connectToDB() {
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
+    const db = client.db('bookingsDB');
+    db.client = client;
+    return db;
+}
+
+module.exports = { connectToDB, ObjectId };
